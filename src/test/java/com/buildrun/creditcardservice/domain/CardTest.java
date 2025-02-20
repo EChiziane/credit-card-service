90c9e334aa3f3d5baac2ab82970481608d0063e1
Chiziane <eddybruno43@gmail.com>
2025-02-20 15:04:10 +0200

    feat()Card Test Implementation

--- src/test/java/com/buildrun/creditcardservice/domain/CardTest.java
+++ src/test/java/com/buildrun/creditcardservice/domain/CardTest.java
@@ -1,10 +1,12 @@
 package com.buildrun.creditcardservice.domain;
+
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.doReturn;
 
@@ -18,42 +20,39 @@ class CardTest {
     private Card card;
 
     @Nested
-    class isBasicCreditCardAvailable {
-        @Test
-        void shouldBeAvailableWhenIncomeLowerThan2kAndScoreGreaterThan500() {
+    class BasicCreditCardAvailabilityTests {
 
+        @Test
+        void shouldBeAvailableWhenIncomeIsBelow2kAndScoreIsAbove500() {
             doReturn(true).when(customer).isIncomeEqualOrLowerThan(2000.0);
             doReturn(true).when(customer).isScoreGreaterThan(500.0);
 
             assertTrue(card.isBasicCreditCardAvailable());
-
         }
-
     }
 
     @Nested
-    class isGoldCreditCardAvailable {
+    class GoldCreditCardAvailabilityTests {
+
         @Test
-        void shouldBeAvailableWhenIncomeBetween2kAnd5kAndScoreGreaterThan600() {
+        void shouldBeAvailableWhenIncomeIsBetween2kAnd5kAndScoreIsAbove600() {
             doReturn(true).when(customer).isIncomeEqualOrLowerThan(5000.0);
             doReturn(true).when(customer).isScoreGreaterThan(600.0);
             doReturn(true).when(customer).isIncomeEqualOrGreaterThan(2000.0);
+
             assertTrue(card.isGoldCreditCardAvailable());
-
         }
-
     }
 
     @Nested
-    class isPlatinumCreditCardAvailable {
+    class PlatinumCreditCardAvailabilityTests {
+
         @Test
-        void shouldBeAvailableWhenIncomeIsEqualOrGreaterThan5kAndScoreGreaterThan() {
+        void shouldBeAvailableWhenIncomeIsAbove5kAndScoreIsAbove750() {
             doReturn(true).when(customer).isScoreGreaterThan(750.0);
             doReturn(true).when(customer).isIncomeEqualOrGreaterThan(5000.0);
+
             assertTrue(card.isPlatinumCreditCardAvailable());
-
         }
-
     }
-
 }
