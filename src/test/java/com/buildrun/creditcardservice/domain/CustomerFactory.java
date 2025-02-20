5c7abff2ede9eabe9552a90b02ba8a454aef070d
Chiziane <eddybruno43@gmail.com>
2025-02-20 13:49:56 +0200

    feat()Customer Test Implementation

---
+++ src/test/java/com/buildrun/creditcardservice/domain/CustomerFactory.java
@@ -0,0 +1,30 @@
+package com.buildrun.creditcardservice.domain;
+
+public class CustomerFactory {
+
+    public static  Customer build(){
+        return   new Customer(
+                "Joel Langa",
+                "123.456.89",
+                3000.0,
+                1000);
+    }
+
+    public static  Customer build(Double income){
+        return   new Customer(
+                "Joel Langa",
+                "123.456.89",
+                income,
+                1000);
+    }
+
+    public static  Customer build(Integer score){
+        return   new Customer(
+                "Joel Langa",
+                "123.456.89",
+                3000.0,
+                score);
+    }
+
+
+}
--- src/test/java/com/buildrun/creditcardservice/domain/CustomerTest.java
+++ src/test/java/com/buildrun/creditcardservice/domain/CustomerTest.java
@@ -8,43 +8,99 @@ import static org.junit.jupiter.api.Assertions.*;
 class CustomerTest {
 
     @Nested
-    class isIncomeEqualOrLowerThan{
+    class isIncomeEqualOrGreaterThan {
+
         @Test
-        void shouldBeTrueWhenIncomeIsEqual(){
-            var customer= new Customer(
-                    "Joel Langa",
-                    "123.456.89",
-                    3000.0,
-                    1000);
-
-        assertTrue(customer.isIncomeEqualOrGreaterThan(3000.0));
-
+        void shouldReturnTrueWhenIncomeIsEqual() { // Nome melhorado
+            var customer = CustomerFactory.build();
+            assertTrue(customer.isIncomeEqualOrGreaterThan(3000.0));
         }
 
         @Test
-        void shouldBeTrueWhenIncomeIsGreaterThan(){
-            var customer= new Customer(
-                    "Joel Langa",
-                    "123.456.89",
-                    3000.0,
-                    1000);
-
+        void shouldReturnTrueWhenIncomeIsGreater() { // Nome melhorado
+            var customer = CustomerFactory.build();
             assertTrue(customer.isIncomeEqualOrGreaterThan(2000.0));
-
         }
 
         @Test
-        void shouldBeFalseWhenIncomeIsLowerThan(){
-            var customer= new Customer(
-                    "Joel Langa",
-                    "123.456.89",
-                    3000.0,
-                    1000);
-
+        void shouldReturnFalseWhenIncomeIsLower() { // Nome melhorado
+            var customer = CustomerFactory.build();
             assertFalse(customer.isIncomeEqualOrGreaterThan(4000.0));
+        }
+    }
 
+    @Nested
+    class isIncomeEqualOrLowerThan {
+
+        @Test
+        void shouldReturnTrueWhenIncomeIsEqual() { // Nome melhorado + correção de "costumer" -> "customer"
+            var customer = CustomerFactory.build();
+            assertTrue(customer.isIncomeEqualOrLowerThan(3000.0));
         }
 
+        @Test
+        void shouldReturnTrueWhenIncomeIsLower() { // Nome melhorado
+            var customer = CustomerFactory.build();
+            assertTrue(customer.isIncomeEqualOrLowerThan(4000.0));
+        }
+
+        @Test
+        void shouldReturnFalseWhenIncomeIsGreater() { // Nome melhorado
+            var customer = CustomerFactory.build();
+            assertFalse(customer.isIncomeEqualOrLowerThan(1000.0));
+        }
     }
+
+    @Nested
+    class isIncomeBetween {
+
+        @Test
+        void shouldReturnTrueWhenIncomeIsWithinRange() { // Nome melhorado
+            var customer = CustomerFactory.build(3000.0);
+            assertTrue(customer.isIncomeBetween(2000, 5000));
+        }
+
+        @Test
+        void shouldReturnTrueWhenIncomeEqualsMinBoundary() { // Nome melhorado
+            var customer = CustomerFactory.build(2000.0);
+            assertTrue(customer.isIncomeBetween(2000, 5000));
+        }
+
+        @Test
+        void shouldReturnTrueWhenIncomeEqualsMaxBoundary() { // Nome melhorado
+            var customer = CustomerFactory.build(5000.0);
+            assertTrue(customer.isIncomeBetween(2000, 5000));
+        }
+
+        @Test
+        void shouldReturnFalseWhenIncomeIsOutOfRange() { // Nome melhorado
+            var customer = CustomerFactory.build(5000.0);
+            assertFalse(customer.isIncomeBetween(2000, 3000));
+        }
+    }
+
+    @Nested
+    class isScoreGreaterThan {
+
+        @Test
+        void shouldReturnTrueWhenScoreIsGreater() { // Nome melhorado
+            var customer = CustomerFactory.build(2000);
+            assertTrue(customer.isScoreGreaterThan(1000.0));
+        }
+
+        @Test
+        void shouldReturnFalseWhenScoreIsLower() { // Nome melhorado
+            var customer = CustomerFactory.build(1000);
+            assertFalse(customer.isScoreGreaterThan(2000.0));
+        }
+
+        @Test
+        void shouldReturnFalseWhenScoreIsEqual() { // Nome melhorado
+            var customer = CustomerFactory.build(1000);
+            assertFalse(customer.isScoreGreaterThan(1000.0));
+        }
+    }
+
+
 
 }
